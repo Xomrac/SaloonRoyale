516da749235f0487ef4e816f305467145b575680
Luca Raffo <55745404+Lucaffo@users.noreply.github.com>
2022-06-22 11:38:11 +0200

    Imported text mesh pro, start state, fixed check player state logic

--- SaloonRoyale/Assets/Scripts/Core/States/CheckPlayerState.cs
+++ SaloonRoyale/Assets/Scripts/Core/States/CheckPlayerState.cs
@@ -2,25 +2,27 @@ namespace Core.States
 {
     public class CheckPlayerState : State
     {
-        private Health _playerHealth;
+        private Character _playerCharacter;
+        
         public override void OnEnter(StateMachine stateMachine)
         {
-            //
+            var healthComponent = _playerCharacter.health;
+            var currentHealth = healthComponent.GetCurrentLife();
+            
+            if (currentHealth <= 0)
+            {
+                stateMachine.ChangeState(stateMachine.endGameState);
+            }
         }
 
         public override void OnUpdate(StateMachine stateMachine)
         {
-            var currentHealth = _playerHealth.GetCurrentLife();
-
-            if (currentHealth <= 0)
-            {
-                stateMachine.ChangeState(new EndGameState());
-            }
+            // Not implemented
         }
 
         public override void OnExit(StateMachine stateMachine)
         {
-            //
+            // Not implemented
         }
     }
 }
--- SaloonRoyale/Assets/Scripts/Core/States/StartGameState.cs
+++ SaloonRoyale/Assets/Scripts/Core/States/StartGameState.cs
@@ -1,20 +1,81 @@
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
 namespace Core.States
 {
     public class StartGameState : State
     {
+        [SerializeField] private CanvasGroup startGameCanvasGroup;
+        
+        [Title("UI Fade In Setting")]
+        [SerializeField] public AnimationCurve fadeInUICurve;
+        [SerializeField] public float fadeInUITime = 2f;
+        
+        [Title("UI Fade Out Settings")]
+        [SerializeField] public AnimationCurve fadeOutUICurve;
+        [SerializeField] public float fadeOutUITime = 2f;
+
+        private Coroutine _canvasGroupCoroutine;
+        
         public override void OnEnter(StateMachine stateMachine)
         {
-            throw new System.NotImplementedException();
+            if (_canvasGroupCoroutine != null)
+            {
+                StopCoroutine(_canvasGroupCoroutine);
+            }
+
+            _canvasGroupCoroutine = StartCoroutine(ShowGroup());
         }
 
         public override void OnUpdate(StateMachine stateMachine)
         {
-            throw new System.NotImplementedException();
+            if (Input.anyKeyDown)
+            {
+                stateMachine.ChangeState(stateMachine.playerState);
+            }
         }
 
         public override void OnExit(StateMachine stateMachine)
         {
-            throw new System.NotImplementedException();
+            if (_canvasGroupCoroutine != null)
+            {
+                StopCoroutine(_canvasGroupCoroutine);
+            }
+
+            _canvasGroupCoroutine = StartCoroutine(HideGroup());
+        }
+
+        private IEnumerator ShowGroup()
+        {
+            var timer = 0f;
+            var t = 0f;
+
+            while (timer <= fadeInUITime)
+            {
+                timer += Time.deltaTime;
+                t = timer / fadeInUITime;
+                startGameCanvasGroup.alpha = fadeInUICurve.Evaluate(t);
+                yield return null;
+            }
+
+            startGameCanvasGroup.alpha = 1.0f;
+        }
+
+        private IEnumerator HideGroup()
+        {
+            var timer = 0f;
+            var t = 0f;
+
+            while (timer <= fadeOutUITime)
+            {
+                timer += Time.deltaTime;
+                t = 1.0f - (timer / fadeOutUITime);
+                startGameCanvasGroup.alpha = fadeInUICurve.Evaluate(t);
+                yield return null;
+            }
+
+            startGameCanvasGroup.alpha = 0.0f;
         }
     }
 }
