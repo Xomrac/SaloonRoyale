f1275881596d86566233fa61660179f1e98ee3c7
Kristi Rrapaj <74373101+kristirrapaj@users.noreply.github.com>
2022-06-24 01:32:30 +0200

    fix ui cards

--- SaloonRoyale/Assets/Scripts/CardSystem/Scriptables/Cards/Card.cs
+++ SaloonRoyale/Assets/Scripts/CardSystem/Scriptables/Cards/Card.cs
@@ -7,12 +7,15 @@ namespace CardSystem
 	public abstract class Card : ScriptableObject
 	{
 		[SerializeField] private Suits cardSuit;
+		[SerializeField] private Sprite suitImage;
 		[SerializeField] private Sprite cardImage;
 		[SerializeField] private string cardName;
 		[SerializeField] private string cardEffect;
 		[SerializeField] private Sprite cardBackground;
 		
 		public Sprite CardImage => cardImage;
+
+		public Sprite SuitImage => suitImage;
 		public Suits CardSuit => cardSuit;
 		public string CardName => cardName;
 
